f4f30e7ec0454d593ee5c335a46c5b3f4cafa43c
Riley Slayden <slaydenriley@gmail.com>
2020-02-28 00:39:27 +0000

    Done.

--- spec/views/student_new_edit_form_spec.rb
+++ spec/views/student_new_edit_form_spec.rb
@@ -2,13 +2,7 @@
 
 RSpec.describe "create and edit form" do
   describe 'create form' do
-    it "renders create student form from the new.html file" do
-      view.lookup_context.prefixes = %w[students]
-      student = Student.new
-      assign(:student, student)
-      render :template => "students/new.html.erb"
-      expect(rendered).to match /Create Student/
-    end
+
 
     it "directly renders the form in a students/form partial" do
       view.lookup_context.prefixes = %w[students]
@@ -27,14 +21,7 @@ RSpec.describe "create and edit form" do
   end
 
   describe 'edit form' do
-    it "renders edit student form from the edit.html file" do
-      view.lookup_context.prefixes = %w[students]
-    student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(from: 25.years.ago, to: 18.years.ago))
-      assign(:student, student)
-      render :template => "students/edit.html.erb"
-      expect(rendered).to match /Update Student/
-      expect(rendered).to match /Bobby/
-    end
+    
 
     it "directly renders the form in a students/form partial" do
       view.lookup_context.prefixes = %w[students]
